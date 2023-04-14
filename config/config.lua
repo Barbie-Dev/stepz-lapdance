7e8e504254dad98b66918428cc190aa4c74c15ad
Chathura Denuwan <92396752+denuwanahackzzz@users.noreply.github.com>
2023-04-15 05:29:54 +0530

    Add files via upload

---
+++ config/config.lua
@@ -0,0 +1,15 @@
+Config = {}
+
+Config['PoleDance'] = { 
+    ['Enabled'] = true,
+    ['Locations'] = {
+        -- Gabz Vanilla Unicorn
+        {['Position'] = vector3(104.67, -1294.49, 29.25), ['Number'] = '3'},
+        {['Position'] = vector3(108.76, -1288.96, 29.25), ['Number'] = '1'},
+        {['Position'] = vector3(102.15, -1290.16, 29.25), ['Number'] = '2'},
+    }
+}
+
+Strings = {
+    ['Pole_Dance'] = '[~p~E~w~] Dans et!',
+}
---
+++ fxmanifest.lua
@@ -0,0 +1,16 @@
+fx_version 'cerulean'
+game 'gta5'
+
+
+author 'StepZ Development'
+description 'StepZ Lap Dance'
+version '1.0.0'
+
+
+shared_scripts {
+    'config/config.lua'
+}
+
+client_scripts {
+	'client/*.lua'
+}
